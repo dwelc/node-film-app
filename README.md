4f93362b4bf97f668661833ea4cb4ac56c6f7c7b
Dan Welch <magichours@gmail.com>
2019-04-10 23:17:29 +0100

    Added Readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Node Film App
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const mongodb = require('mongodb')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017')
 
 app.use(cors())
 
